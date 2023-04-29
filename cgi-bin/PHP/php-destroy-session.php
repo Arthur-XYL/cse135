3637a4a3c3af6058a6cce1876b0cfc2e69056f30
Yilong Xiong <yxiong@ucsd.edu>
2023-04-29 18:09:05 +0000

    add php sessions

--- cgi-bin/PHP/php-destroy-session.php
+++ cgi-bin/PHP/php-destroy-session.php
@@ -8,7 +8,7 @@ echo "<title>PHP Session Destroyed</title>";
 echo "</head>";
 echo "<body>";
 echo "<h1>Session Destroyed</h1>";
-echo "<a href=\"/cgi-forms/php-cgiform.html\">Back to the PHP Form</a><br />";
+echo "<a href=\"/cgi-forms/php-cgiform.html\">Back to the PHP CGI Form</a><br />";
 echo "<a href=\"/cgi-bin/PHP/php-sessions-1.php\">Back to Page 1</a><br />";
 echo "<a href=\"/cgi-bin/PHP/php-sessions-2.php\">Back to Page 2</a>";
 echo "</body>";
