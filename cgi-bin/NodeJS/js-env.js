43015b5b365b8effad9af144fd335eb1001b350d
RuoqianHuang <r8huang@ucsd.edu>
2023-04-29 18:52:20 -0700

    edit js env 3

--- cgi-bin/NodeJS/js-env.js
+++ cgi-bin/NodeJS/js-env.js
@@ -5,16 +5,14 @@ const http = require('http');
 process.stdout.write('Cache-Control: no-cache\n');
 process.stdout.write('Content-Type: text/html\n\n');
 
-process.stdout.write('<!DOCTYPE html>');
 process.stdout.write('<html><head><title>Environment Variables</title>');
 process.stdout.write('</head><body><h1 align="center">Environment Variables</h1>');
 process.stdout.write('<hr>');
 
 // Loop over the environment variables and print each variable and its value
 for (const variable in process.env) {
-    process.stdout.write(`<b>${variable}:</b> <br/>`);
+    process.stdout.write(`<b>${variable}:</b> ${process.env[variable]}<br/>`);
 }
-console.log(process.env);
 
 process.stdout.write('</body>');
 process.stdout.write('</html>');
