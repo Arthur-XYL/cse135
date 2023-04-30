66c33fd6f271f124d85cd92f0d553e309a1981bb
RuoqianHuang <r8huang@ucsd.edu>
2023-04-29 19:29:19 -0700

    js post echo

---
+++ cgi-bin/NodeJS/js-post-echo.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const readline = require('readline');
+
+// Print HTTP headers
+process.stdout.write('Cache-Control: no-cache\n');
+process.stdout.write('Content-type: text/html\n\n');
+
+// Start HTML document
+process.stdout.write('<html><head><title>POST Message Body</title></head>');
+process.stdout.write('<body><h1 align="center">POST Message Body</h1><hr>');
+
+// Read and print POST data
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    process.stdout.write(`Message Body: ${input}<br/>\n`);
+});
+
+// End HTML document
+process.stdout.write('</body></html>');
