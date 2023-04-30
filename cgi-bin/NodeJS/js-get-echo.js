1eb0b505b7acab0f287095f2bfbeb1fcee7f210c
Yilong Xiong <yxiong@ucsd.edu>
2023-04-30 04:42:12 +0000

    fix js duplicates

--- cgi-bin/NodeJS/js-get-echo.js
+++ cgi-bin/NodeJS/js-get-echo.js
@@ -22,30 +22,3 @@ for (const key in query) {
     process.stdout.write('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
 }
 process.stdout.write('</table>');
-#!/usr/bin / env node
-
-const http = require('http');
-const url = require('url');
-
-// Set HTTP headers
-process.stdout.write('Cache-Control: no-cache\n');
-process.stdout.write('Content-type: text/html\n\n');
-
-// Start HTML document
-process.stdout.write('<html><head><title>GET Request Echo</title></head>');
-process.stdout.write('<body><h1 align="center">GET Request Echo</h1><hr>');
-
-// Parse the query string
-const query = url.parse(process.env.REQUEST_URI, true).query;
-
-// Print the query string
-process.stdout.write('<b>Query String:</b> ' + process.env.QUERY_STRING + '<br><br>');
-process.stdout.write('<table><tr><th>Variable</th><th>Value</th></tr>');
-for (const key in query) {
-    const value = query[key];
-    process.stdout.write('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
-}
-process.stdout.write('</table>');
-
-// End HTML document
-process.stdout.write('</body></html>');
--- cgi-bin/NodeJS/js-post-echo.js
+++ cgi-bin/NodeJS/js-post-echo.js
@@ -24,29 +24,4 @@ rl.on('line', (input) => {
 
 // End HTML document
 process.stdout.write('</body></html>');
-#!/usr/bin / env node
 
-const readline = require('readline');
-
-// Print HTTP headers
-process.stdout.write('Cache-Control: no-cache\n');
-process.stdout.write('Content-type: text/html\n\n');
-
-// Start HTML document
-process.stdout.write('<html><head><title>POST Message Body</title></head>');
-process.stdout.write('<body><h1 align="center">POST Message Body</h1><hr>');
-
-process.stdout.write('<b>Message Body: </b>');
-
-// Read and print POST data
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.on('line', (input) => {
-    process.stdout.write(input + '<br/>');
-});
-
-// End HTML document
-process.stdout.write('</body></html>');
