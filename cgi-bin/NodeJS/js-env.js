22840c747a722126748be1fc3e4f62173b1624bc
Yilong Xiong <yxiong@ucsd.edu>
2023-04-30 04:13:24 +0000

    hw2 part 2 step 2

---
+++ cgi-bin/NodeJS/js-env.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const http = require('http');
+
+process.stdout.write('Cache-Control: no-cache\n');
+process.stdout.write('Content-Type: text/html\n\n');
+
+process.stdout.write('<html><head><title>Environment Variables</title>');
+process.stdout.write('</head><body><h1 align="center">Environment Variables</h1>');
+process.stdout.write('<hr>');
+
+// Loop over the environment variables and print each variable and its value
+for (const variable in process.env) {
+    process.stdout.write(`<b>${variable}:</b> ${process.env[variable]}<br/>`);
+}
+
+process.stdout.write('</body>');
+process.stdout.write('</html>');
