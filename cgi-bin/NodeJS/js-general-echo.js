8a191805683b5b8b6cd5eb191bfd7431b3706077
RuoqianHuang <r8huang@ucsd.edu>
2023-04-29 22:34:54 -0700

    edit js general

--- cgi-bin/NodeJS/js-general-echo.js
+++ cgi-bin/NodeJS/js-general-echo.js
@@ -3,12 +3,12 @@
 const readline = require('readline');
 
 // Print HTTP headers
-process.stdout.write("Cache-Control: no-cache");
-process.stdout.write("Content-type: text/html\n");
+process.stdout.write("Cache-Control: no-cache\n");
+process.stdout.write("Content-type: text/html\n\n");
 
 // Start HTML document
-process.stdout.write("<html><head><title>General Request Echo</title></head>");
-process.stdout.write("<body><h1 align=\"center\">General Request Echo</h1><hr>");
+process.stdout.write('<html><head><title>General Request Echo</title></head>');
+process.stdout.write('<body><h1 align="center">General Request Echo</h1><hr>');
 
 // From process.env we can set the environment variables:
 process.env.SERVER_PROTOCOL = 'HTTP/1.1';
@@ -20,13 +20,14 @@ process.stdout.write(`<p><b>HTTP Protocol:</b> ${process.env.SERVER_PROTOCOL}</p
 process.stdout.write(`<p><b>HTTP Method:</b> ${process.env.REQUEST_METHOD}</p>`);
 process.stdout.write(`<p><b>Query String:</b> ${process.env.QUERY_STRING}</p>`);
 
+process.stdout.write(`<b>Message Body:</b>`);
+
 // Read and print message body from standard input
 const rl = readline.createInterface({
   input: process.stdin,
   terminal: false
 });
 
-process.stdout.write(`<b>Message Body:</b>`);
 rl.on('line', (line) => {
     process.stdout.write(line + + '<br/>');
 });
