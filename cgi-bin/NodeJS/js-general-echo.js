1d8e707649b7219f46072ca01ed235c1f1ee080d
RuoqianHuang <r8huang@ucsd.edu>
2023-04-29 22:19:45 -0700

    edit js general

--- cgi-bin/NodeJS/js-general-echo.js
+++ cgi-bin/NodeJS/js-general-echo.js
@@ -10,6 +10,11 @@ process.stdout.write("Content-type: text/html\n");
 process.stdout.write("<html><head><title>General Request Echo</title></head>");
 process.stdout.write("<body><h1 align=\"center\">General Request Echo</h1><hr>");
 
+// From process.env we can set the environment variables:
+process.env.SERVER_PROTOCOL = 'HTTP/1.1';
+process.env.REQUEST_METHOD = 'GET';
+process.env.QUERY_STRING = '';
+
 // Print HTTP protocol, method and query string
 process.stdout.write(`<p><b>HTTP Protocol:</b> ${process.env.SERVER_PROTOCOL}</p>`);
 process.stdout.write(`<p><b>HTTP Method:</b> ${process.env.REQUEST_METHOD}</p>`);
@@ -21,8 +26,9 @@ const rl = readline.createInterface({
   terminal: false
 });
 
+process.stdout.write(`<b>Message Body:</b>`);
 rl.on('line', (line) => {
-    process.stdout.write(`<p><b>Message Body:</b> ${line}</p>`);
+    process.stdout.write(line + + '<br/>');
 });
 
 // End HTML document
